b17885e092d78619eaa6d8a8b61743e2003667ae
Moritz Schuck <159003684+MoJoSchuck@users.noreply.github.com>
2025-07-18 13:07:33 +0200

    Public application to GitHub

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,38 @@
 import express from "express";
-import notesRoutes from "./routes/notesRoutes.js"
+import cors from "cors";
+import dotenv from "dotenv";
+
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001
+
+// middleware
+app.use(
+    cors({
+        origin: "http://localhost:5173"
+
+    }));
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter);
+
+
+// app.use((req, res, next) => {
+//     console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//     next();
+// });
 
 app.use("/api/notes", notesRoutes)
 
-app.listen(5001, () => {
-    console.log("Server started on PORT: 5001");
+
+connectDB().then(() => {
+
+    app.listen(PORT, () => {
+        console.log("Server started on PORT:", PORT);
+    });
 });
+
